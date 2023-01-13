99212a3a8785764a88bd4290314c3764c41965d3
Thera <38962379+Dereksidi@users.noreply.github.com>
2023-01-13 10:48:57 -0400

    Solving Issues

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ module.exports = {
   env: {
     NEXT_PUBLIC_PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
     NEXT_PUBLIC_PROJECT_SECRET: process.env.NEXT_PUBLIC_PROJECT_SECRET,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
   },
   images: {
     domains: ['pikblock.infura-ipfs.io', 'infura-ipfs.io'],
